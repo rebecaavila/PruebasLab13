1416b02a0536eae550f78c81649260cc58a705a2
Edwin Chigne <98482516+HackerenProceso@users.noreply.github.com>
2024-06-09 17:50:07 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@
 
 -- Crear tabla "clientes" para almacenar información de clientes
 CREATE TABLE clientes (
-  id_cliente SERIAL PRIMARY KEY,  -- Definir id_cliente como clave primaria
+  id_cliente INTEGER PRIMARY KEY,  -- Definir id_cliente como clave primaria
   nombre VARCHAR(255) NOT NULL,
   apellido VARCHAR(255) NOT NULL,
   direccion VARCHAR(255),
@@ -14,19 +14,19 @@ CREATE TABLE clientes (
 
 -- Crear tabla "mascotas" para almacenar información de mascotas
 CREATE TABLE mascotas (
-  id_mascota SERIAL PRIMARY KEY,
+  id_mascota INTEGER PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   especie VARCHAR(50) NOT NULL,
   raza VARCHAR(50),
   sexo VARCHAR(10),
   fecha_nacimiento DATE,
-  id_cliente INTEGER NOT NULL,  -- Cambiar INTEGER a SERIAL
+  id_cliente INTEGER NOT NULL,
   FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
 -- Crear tabla "citas" para almacenar información de citas
 CREATE TABLE citas (
-  id_cita SERIAL PRIMARY KEY,
+  id_cita INTEGER PRIMARY KEY,
   fecha_hora TIMESTAMP NOT NULL,
   motivo_consulta VARCHAR(255),
   id_mascota INTEGER NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE citas (
 
 -- Crear tabla "historial_medico" para almacenar el historial médico de las mascotas
 CREATE TABLE historial_medico (
-  id_historial SERIAL PRIMARY KEY,
+  id_historial INTEGER PRIMARY KEY,
   id_cita INTEGER NOT NULL,
   diagnostico VARCHAR(255),
   tratamiento VARCHAR(255),
